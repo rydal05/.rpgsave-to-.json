d97d87f4c7261f2a8804062e83364b822e903e6e
khanchi <khanchi@RYAND>
2025-05-31 14:34:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using LZStringCSharp;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "../../../input/file1.rpgsave";
+            //Console.WriteLine(File.Exists(path)); returned true
+            
+        }
+    }
+}
